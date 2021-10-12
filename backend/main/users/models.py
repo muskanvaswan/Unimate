52d0461165021ceb1b6f19c75a2a1837b89edd29
sidP28 <siddhantpoddar28@gmail.com>
2021-10-12 16:47:01 +0530

    added models

--- backend/main/users/models.py
+++ backend/main/users/models.py
@@ -7,4 +7,30 @@ class user(models.Model):
     location = models.TextField()
     phone = models.BigIntegerField()
 
-    
+class scores(models.Model):
+    SAT= models.IntegerField()
+    ACT= models.IntegerField()
+    GRE=models.IntegerField()
+    GMAT=models.IntegerField()
+    UG=models.IntegerField()
+    HS=models.IntegerField()
+
+class colleges(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.TextField()
+    rank = models.TextField()
+    reviews = models.TextField()
+    acc_rate = models.CharField(max_length=50)
+    deadlines = models.TextField()
+
+class tracker(models.Model):
+    college = models.CharField(max_length=100)
+    deadlines = models.TextField()
+    completed = models.TextField()
+    criteria = models.TextField()
+
+class preferences(models.Model):
+    country = models.CharField(max_length=20)
+    rank = models.IntegerField()
+    course = models.TextField()
