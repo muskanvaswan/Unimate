89a89f223a28daf511333bd3cb236e1a4e2bb9da
Muskan Vaswan <muskanvaswan@gmail.com>
2021-11-08 13:50:55 +0530

    add college data

--- backend/backend/users/models.py
+++ backend/backend/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class College(models.Model):
+    world_rank = models.IntegerField()
+    name = models.CharField(max_length=120)
+    country = models.CharField(max_length=120)
+    teaching = models.DecimalField(max_digits=19, decimal_places=10)
+    international = models.DecimalField(max_digits=19, decimal_places=10)
+    research = models.DecimalField(max_digits=19, decimal_places=10)
+    citations = models.DecimalField(max_digits=19, decimal_places=10)
+    income = models.DecimalField(max_digits=19, decimal_places=10)
+    total_score = models.DecimalField(max_digits=19, decimal_places=10)
+    num_students = models.IntegerField()
