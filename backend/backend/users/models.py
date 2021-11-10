b13aef3314e6646d5da515658ea70c830e120b1d
Muskan Vaswan <muskanvaswan@gmail.com>
2021-11-10 21:11:34 +0530

    add new models for tracking

--- backend/backend/users/models.py
+++ backend/backend/users/models.py
@@ -13,7 +13,19 @@ class College(models.Model):
     total_score = models.DecimalField(max_digits=19, decimal_places=10)
     num_students = models.IntegerField()
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     colleges = models.ManyToManyField(College,  related_name="colleges")
     sat_score = models.IntegerField(null=True)
+
+class Tracker(model.Mode):
+    user = models.ForiegnKeyField(User, on_delete=models.CASCADE, related_name="tracker")
+    college = models.ForiegnKeyField(College, on_delete=models.CASCADE, related_name="tracker")
+    deadline = models.ManyToManyField(Deadline, related_name="deadlines")
+    category = models.CharField()
+
+class Deadline(models.Model):
+    date = models.DateTimeField()
+    title = models.CharField(max_length=200)
+    status = models.BooleanField(default=False)
