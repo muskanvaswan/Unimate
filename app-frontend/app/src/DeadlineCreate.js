589838a9c8a778467fe30b51948210416288624c
Muskan Vaswan <muskanvaswan@gmail.com>
2021-11-12 15:08:42 +0530

    made dealine create component

---
+++ app-frontend/app/src/DeadlineCreate.js
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import { Button, IconButton, TextInput, useTheme, Text } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import DatePicker from 'react-native-date-picker'
+
+const MyComponent = (props) => {
+
+  const { colors } = useTheme();
+
+  const [ title, setTitle ] = React.useState('')
+  const [ open, setOpen ] = React.useState(false)
+  const [ date, setDate ] = React.useState(new Date())
+
+  const createDeadline = () => {
+
+    props.setVisible(false);
+  }
+  return (
+    <View style={[styles.container, {backgroundColor: colors.background,}]}>
+      <Text style={styles.heading}>Add a deadline</Text>
+      <Button
+        icon="calendar"
+        onPress={() => setOpen(true)}
+        mode="outlined"
+        color={colors.highlight}
+        >
+        Set Deadline End Date
+      </Button>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput
+          value={title}
+          style={styles.input}
+          mode="outlined"
+          onChangeText={title => setTitle(title)}
+          outlineColor={colors.highlight}
+        />
+        <IconButton
+          icon="check"
+          color={colors.accent}
+          size={30}
+          mode="contained"
+          onPress={createDeadline}
+        />
+      </View>
+      <DatePicker
+        modal
+        open={open}
+        date={date}
+        onConfirm={(date) => {
+          setOpen(false)
+          setDate(date)
+        }}
+        onCancel={() => {
+          setOpen(false)
+        }}
+      />
+
+    </View>
+  )
+};
+
+export default MyComponent
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 50,
+    marginTop: 10,
+    padding: 15,
+    borderRadius: 10,
+
+    position: 'absolute',
+    top: '60%',
+    width: '100%',
+    zIndex: 2,
+  },
+  input: {
+    width: '85%',
+    height: 40,
+    marginVertical: 10,
+    borderRadius: 50,
+    backgroundColor: 'rgba(128, 126, 126, 0.03)'
+  },
+  heading: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  }
+
+});
